149ce602c21d35f8974b065689486b3fa109432d
qMaDMaNp <medifman@gmail.com>
2023-09-16 14:22:25 -0400

    Add sidebar with categories

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import { Sidebar } from '../components';
+import { categories } from '../utils/constants';
 
 function Feed() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0].name);
+
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{
+        height: { sx: "auto", md: "92vh" },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 }
+      }}>
+        <Sidebar {...{ selectedCategory, setSelectedCategory }} />
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
+import './utils/fetchFromApi';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
