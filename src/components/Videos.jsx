f52c0105fad6f27149b6c3ba2c12465032026af2
qMaDMaNp <medifman@gmail.com>
2023-09-17 21:36:13 -0400

    Add ability to watch videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,14 +3,14 @@ import { Stack, Box } from "@mui/material";
 
 import { ChannelCard, Loader, VideoCard } from "./";
 
-function Videos({ videos }) {
-  if(!videos?.length) return <Loader />;
-  
+function Videos({ videos, direction }) {
+  if (!videos?.length) return <Loader />;
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((video, idx) => (
         <Box key={idx}>
-          {video.id.videoId && <VideoCard video={video} /> }
+          {video.id.videoId && <VideoCard video={video} />}
           {video.id.channelId && <ChannelCard channelDetails={video} />}
         </Box>
       ))}
